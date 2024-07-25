9a281e0d87b5c9c7cb0d8f8e83dd2314661326c0
Sacheen <sachin.shrestha42@gmail.com>
2024-07-26 08:58:22 +1000

    pkl

--- backtest/backtest_check.py
+++ backtest/backtest_check.py
@@ -3,67 +3,72 @@ from src.Technicals import Indicator
 from src.Trademanager import Trademanager
 import pandas as pd
 
-# Load and preprocess data
+class backtester():
+    @staticmethod
+    def strategy(df):
+        # Assuming Indicator methods are static methods or accessible directly
+        baseline_df = Indicator().Baseline(data=df, period=8)
+        ssl_df = Indicator().SSL(data=baseline_df, period=10)
+        atr_df = Indicator().ATR(data=ssl_df, timeperiod=14)
+        wae_df = Indicator().WAE(df=atr_df)
+        return wae_df
 
-df = pd.read_csv(r"C:\Users\sachi\OneDrive\Documents\BOTS\SELENIUM\data_check\ACLBSL_data.csv")
+    @staticmethod
+    def backtest_forex(df):
+        entry_price = None
+        trade_type = None
+        pips_gained = []
+        for index, row in df.iterrows():
+            if row['FINAL_SIGNAL_shift'] == 'SELL' or row['FINAL_SIGNAL_shift'] == 'BUY':
+                if entry_price is None:
+                    entry_price = row['Close']
+                    trade_type = row['FINAL_SIGNAL_shift']
+                    print(entry_price, trade_type)
+                elif entry_price is not None:
+                    exit_price = row['Close']
+                    pips = exit_price - entry_price if trade_type == "SELL" else entry_price - exit_price
+                    pips_gained.append(abs(pips))
+                    print(exit_price, entry_price, abs(pips))
+                    entry_price = row['Close']
+                    trade_type = row['FINAL_SIGNAL_shift']
+                    print(entry_price, trade_type)
+
+        return sum(pips_gained)
+
+    @staticmethod
+    def backtest_stock(df,unit = 10):
+        collection = []
+        collection_units = []
+        gain_amount = []
+        positive_count = 0
+        negative_count = 0
+        total_collection_inv = []
+
+        unit = unit
+        for index, row in df.iterrows():
+            if row['FINAL_SIGNAL'] == "BUY":
+                investment = unit * row['Close']
+                collection_units.append(unit)
+                collection.append(investment)
+            elif row['FINAL_SIGNAL'] == "SELL":
+                if collection:
+                    total_investment = sum(collection)
+                    total_units = sum(collection_units)
+                    total_selling_price = row['Close'] * total_units
+                    gain = total_selling_price - total_investment
+                    gain_amount.append(gain)
+                    total_collection_inv.append(total_investment)
+                    collection = []
+                    collection_units = []
+                    if gain > 0:
+                        positive_count += 1
+                    elif gain < 0:
+                        negative_count += 1
+
+        total_gain = sum(gain_amount)
+        total_trades = positive_count + negative_count
+        win_rate = (positive_count / total_trades * 100) if total_trades > 0 else 0
+
+        return total_gain, positive_count, negative_count, win_rate,total_trades,sum(total_collection_inv)
 
 
-
-# Rest of your code
-def strategy(df):
-    # Apply technical indicators
-    baseline_df = Indicator().Baseline(data=df, period=10)
-    ssl_df = Indicator().SSL(data=baseline_df, period=10)
-    atr_df = Indicator().ATR(data=ssl_df, timeperiod=14)
-    wae_df = Indicator().WAE(df=atr_df)
-    
-    def final_signal(row):
-        long_condition = (
-            row['Close'] > row['baseline'] and 
-            row['SSL_BUY'] and 
-            row['trendUp'] > row['e1']
-        )
-        short_condition = (
-            row['Close'] < row['baseline'] and 
-            row['SSL_SELL'] and 
-            row['trendDown'] > row['e1']
-        )
-
-        if long_condition:
-            return "BUY"
-        elif short_condition:
-            return "SELL"
-        else:
-            return None
-    
-    wae_df['FINAL_SIGNAL'] = wae_df.apply(final_signal, axis=1)
-    return wae_df        
-
-def backtest_func(df):
-    entry_price = None
-    trade_type = None
-    pips_gained = []
-    
-    for index, row in df.iterrows():
-        if row['FINAL_SIGNAL'] in ['SELL', 'BUY']:
-            if entry_price is None:
-                entry_price = row['Close']
-                trade_type = row['FINAL_SIGNAL']
-                print(f"Entry Price: {entry_price}, Trade Type: {trade_type}")
-            else:
-                exit_price = row['Close']
-                pips = exit_price - entry_price if trade_type == "SELL" else entry_price - exit_price
-                pips_gained.append(abs(pips))
-                print(f"Exit Price: {exit_price}, Entry Price: {entry_price}, Pips Gained: {abs(pips)}")
-
-                # Update entry price and trade type
-                entry_price = row['Close']
-                trade_type = row['FINAL_SIGNAL']
-                print(f"Entry Price: {entry_price}, Trade Type: {trade_type}")
-
-    return sum(pips_gained)
-
-if __name__ == "__main__":
-    check_df = strategy(df)
-    total_pips = backtest_func(check_df)
-    print(f"Total Pips Gained: {total_pips}")
--- yfinance_check/data_fetcher.py
+++ yfinance_check/data_fetcher.py
@@ -1,92 +1,65 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
 import yfinance as yf
-import talib
-from backtest import backtest_check
+from backtest.backtest_check import backtester
+import os
 
-df = yf.download(tickers="AAPL", period = "max",interval = "1d")
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        contents = file.read()
+    # Split contents into a list of tickers (assuming each ticker is on a new line)
+    return contents.strip().split('\n')
 
+# Path to the stocks_list.txt file
+file_path = os.path.join('yfinance_check', 'stocks_list.txt')
 
-class backtest():
-  def strategy(df):
-      baseline_df = Indicator().Baseline(data=df, period=10)
-      ssl_df = Indicator().SSL(data=baseline_df, period=10)
-      atr_df = Indicator().ATR(data=ssl_df,timeperiod=14)
-      wae_df=Indicator().WAE(df=atr_df)
-      return wae_df
+# Read the list of tickers from the file
+stocks_list = read_file(file_path)
 
+# Initialize results dictionary
+results_dict = {}
+dataframes = {}
 
-  def backtest_forex(df):
-    entry_price = None
-    trade_type = None
-    pips_gained = []
-    for index, row in df.iterrows():
-      if row['FINAL_SIGNAL'] == 'SELL' or row['FINAL_SIGNAL'] == 'BUY':
-        if entry_price is None:
-          entry_price = row['Close']
-          trade_type = row['FINAL_SIGNAL']
-          print(entry_price,trade_type)
-        elif entry_price is not None:
+# Process each ticker
+for ticker in stocks_list:
+    # Download historical data for the ticker
+    df = yf.download(tickers=ticker, period="max", interval="1d")
+    dataframes[ticker] = df
 
-          exit_price = row['Close']
-          pips = exit_price - entry_price if trade_type == "SELL" else entry_price - exit_price
-          pips_gained.append(abs(pips))
-          print(exit_price,entry_price,abs(pips))
+    # Save DataFrame to a pickle file
+    pickle_path = os.path.join('yfinance_check/stocks_datas', f'{ticker}_data.pkl')
+    df.to_pickle(pickle_path)
 
-          entry_price = row['Close']
-          trade_type = row['FINAL_SIGNAL']
-          print(entry_price,trade_type)
+    if df.empty:
+        print(f"No data available for {ticker}")
+        continue  # Skip processing if data is empty
 
-    return sum(pips_gained)
+    print(f"Download successful for {ticker}")
 
+    # Apply backtesting strategy
+    bt = backtester()
+    bt_df = bt.strategy(df)  # Apply strategy to the dataframe
+    total_gain, positive_count, negative_count, win_rate, total_trades, total_investment_collection = bt.backtest_stock(bt_df, unit=1)
 
+    # Store results in dictionary
+    results_dict[ticker] = {
+        'Total Gain': total_gain,
+        'Positive Gains': positive_count,
+        'Negative Gains': negative_count,
+        'Win %': win_rate,
+        'Total Trades': total_trades,
+        'Total Investment': total_investment_collection
+    }
 
-  def backtest_stock(df):
-      collection = []           # Define inside the function to keep the state within the function scope
-      collection_units = []     # Define inside the function to keep the state within the function scope
-      gain_amount = []
-      positive_count = 0        # Initialize the positive count
-      negative_count = 0        # Initialize the negative count
+    # Print results for each ticker
+    print(f"Ticker: {ticker}")
+    print(f"Total Gain: {total_gain}")
+    print(f"Positive Gains: {positive_count}")
+    print(f"Negative Gains: {negative_count}")
+    print(f"Win %: {win_rate}")
+    print(f"Total Trades: {total_trades}")
+    print(f"Total Investment: {total_investment_collection}\n")
 
-      unit = 10
-      for index, row in df.iterrows():
-          if row['FINAL_SIGNAL'] == "BUY":
-              investment = unit * row['Close']
-              collection_units.append(unit)
-              collection.append(investment)
-          elif row['FINAL_SIGNAL'] == "SELL":
-              if collection:
-                  total_investment = sum(collection)
-                  total_units = sum(collection_units)
-                  total_selling_price = row['Close'] * total_units
-                  gain = total_selling_price - total_investment
-                  gain_amount.append(gain)
-                  print(f"Sold Units: {total_units}")
-                  print(f"Total investment: {total_investment}")
-                  print(f"Total Selling Price: {total_selling_price}")
-                  print(f"Gain from stock: {gain}")
-                  collection = []  # Reset collection after selling
-                  collection_units = []  # Reset units after selling
-                  if gain > 0:
-                      positive_count += 1
-                  elif gain < 0:
-                      negative_count += 1
-
-      total_gain = sum(gain_amount)
-      total_trades = positive_count + negative_count
-      win_rate = (positive_count / total_trades * 100) if total_trades > 0 else 0
-
-      print(f"Total Gain: {total_gain}")
-      print(f"Positive Gains: {positive_count}")
-      print(f"Negative Gains: {negative_count}")
-      print(f"Win %: {win_rate}")
-
-  # Example usage:
-  # Assuming df is your DataFrame with the necessary columns
-  # stock_bt(df)
-
-
-backtest = backtest_check.strategy(df=df)
-print(backtest)
+# Optionally, save results to a file
+results_df = pd.DataFrame(results_dict).T
+results_df.to_csv(os.path.join('yfinance_check', 'backtest_results.csv'))
